e0f87113cb54ded1d2f179f1ac72997714b52e4e
jollen <jollen@moko365.com>
2015-02-15 07:34:54 +0000

    update: send/viewer handling

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,12 +1,50 @@
 function send(pathname, connection, clients) {
     console.log("Routed: " + pathname);
+
+    // the original sender pathname
+    connection.pathname = pathname;
+
+    /*
+     * convert sender pathname to viewer pathname
+     * eg. '/object/mbedtaiwan/send' to '/object/mbedtaiwan/viewer'
+     */
+    var paths = pathname.split('/');
+
+    // remove the rear string 'send'
+    var viewer = paths.slice(0, -1).join('/');
+
+    connection.viewer = viewer + '/viewer';
+
+    /*
+     * initial storage for this viewer
+     */
+    for (var path in clients) {
+        if (path === connection.viewer)
+            return;
+    }
+
+    clients[connection.viewer] = [];
 }
 
 function viewer(pathname, connection, clients) {
     console.log("Routed: " + pathname);
 
-    // Save clients (unlimited clients)
-    clients.push(connection);
+    // the original sender pathname
+    connection.pathname = pathname;
+
+    // Save viewer clients (unlimited clients)
+    for (var path in clients) {
+        if (path === pathname) {
+            clients[path].push(connection);
+            return;
+        }
+    }
+
+    /*
+     * Not found ? There is not a existing sender.
+     */
+    clients[pathname] = [];
+    clients[pathname].push(connection);
 }
 
 exports.send = send;
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var http = require("http");
 var url = require("url");
 
 var WebSocketServer = require('websocket').server;
-var WebSocketRouter = require('websocket').router;
 
 // Connected WebSocket clients
 var clients = [];
@@ -28,32 +27,40 @@ function start(route, handlers) {
     autoAcceptConnections: false
   });
 
-  function push(data) {
-    for (var i = 0; i < clients.length; i++) {
-        clients[i].sendUTF(data);
-    }
+  var push = function(path, data) {
+    var connections = clients[path];
 
-    ////// Dump ////////
-    //console.log( JSON.parse(data) );
+    if (typeof connections === 'undefined')
+        return;
+
+    console.log('Pushing [' + data + '] to ' + path);
+
+    for (var i = 0; i < connections.length; i++) {
+        connections[i].sendUTF(data);
+    }
   }
 
-  function onWsConnMessage(message) {
+  var onWsConnMessage = function(message) {
     if (message.type == 'utf8') {
-      push(message.utf8Data);
+      //console.log('onWsConnMessage: ' + this.pathname);
       //console.log('Received: ' + message.utf8Data);
+
+      // Is it a sender ? Yes, then push data to all viewers.
+      if (typeof (this.viewer) !== 'undefined')
+          push(this.viewer, message.utf8Data);
     } else if (message.type == 'binary') {
       console.log('Received binary data.');
     }
-  }
+  };
 
-  function onWsConnClose(reasonCode, description) {
+  var onWsConnClose = function(reasonCode, description) {
     //console.log('Peer disconnected with reason: ' + reasonCode);
-  }
+  };
 
   function onWsRequest(request) {
     var connection = request.accept('', request.origin);
-    //console.log("[2]: onWsRequest");
 
+    //console.log("[2]: onWsRequest");
     //console.log("[3]: resource: " + request.resource);
 
     route(request.resource, connection, handlers, clients);
@@ -65,8 +72,8 @@ function start(route, handlers) {
   function onWsConnect(webSocketConnection) {
     //console.log("[1]: onWsConnect");
 
-    webSocketConnection.on('message', onWsConnMessage);
-    webSocketConnection.on('close', onWsConnClose);
+    //webSocketConnection.on('message', onWsConnMessage);
+    //webSocketConnection.on('close', onWsConnClose);
   }
 
   wsServer.on('request', onWsRequest);
