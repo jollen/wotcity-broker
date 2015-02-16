6211bc6800e2879edcfed177bf62cf105747bfef
Jollen Chen <jollen@moko365.com>
2015-02-17 00:15:41 +0800

    add: more tests

--- tests/client1.js
+++ tests/client1.js
@@ -19,23 +19,6 @@ client.on('connect', function(connection) {
             console.log("Received: '" + message.utf8Data + "'");
         }
     });
-
-    function sendNumber() {
-        if (connection.connected) {
-            var number = Math.round(Math.random() * 0xFFFFFF);
-            // 1 to 10
-            var lucky = Math.round(Math.random() * 10 + 1);
-            var obj = {ax: number.toString(), ay: 0, az: 0};
-
-            if (lucky === 8)
-                connection.sendUTF("{\"hello\": 0 }");
-            else
-                connection.sendUTF(JSON.stringify(obj));
-
-            setTimeout(sendNumber, 5000);
-        }
-    }
-    sendNumber();
 });
 
-client.connect('ws://wot.city/object/mbedtaiwan/viewer', '');
+client.connect('ws://wot.city/object/test2/viewer', '');
--- tests/test.js
+++ tests/test.js
@@ -26,20 +26,15 @@ client.on('connect', function(connection) {
             // 1 to 10
             var lucky = Math.round(Math.random() * 10 + 1);
             //var obj = {ax: number.toString(), ay: 0, az: 0};
-            var obj = {lowpulseoccupancytime: number};
-
-            if (lucky === 8)
-                //connection.sendUTF("{\"hello\": 0 }");
-                connection.sendUTF(JSON.stringify(obj));
-            else
-                connection.sendUTF(JSON.stringify(obj));
+            var obj = {random: number};
 
             console.log('Pushing: ' + JSON.stringify(obj));
 
+            connection.sendUTF(JSON.stringify(obj));
             setTimeout(sendNumber, 1000);
         }
     }
     sendNumber();
 });
 
-client.connect('ws://wot.city/object/accelerometer/send', '');
+client.connect('ws://wot.city/object/test/send', '');
