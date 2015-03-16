068f53ce3446d168d4e32d9371ef3fa47813bfca
Jollen Chen <jollen@moko365.com>
2015-03-16 18:55:00 +0800

    use cluster

--- server.js
+++ server.js
@@ -1,10 +1,63 @@
 var http = require("http");
 var url = require("url");
+var cluster = require('cluster');
 
 var WebSocketServer = require('websocket').server;
 
 // Connected WebSocket clients
 var clients = [];
+
+/**************************************************
+ * handlers
+ **************************************************/
+var push = function(path, data) {
+  var connections = clients[path];
+
+  if (typeof connections === 'undefined')
+      return;
+
+  //console.log('Pushing [' + data + '] to ' + path);
+
+  for (var i = 0; i < connections.length; i++) {
+      connections[i].sendUTF(data);
+  }
+}
+
+var onWsConnMessage = function(message) {
+  if (message.type == 'utf8') {
+    //console.log('onWsConnMessage: ' + this.pathname);
+    //console.log('Received: ' + message.utf8Data);
+
+    // Is it a sender ? Yes, then push data to all viewers.
+    if (typeof (this.viewer) !== 'undefined')
+        push(this.viewer, message.utf8Data);
+  } else if (message.type == 'binary') {
+    console.log('Received binary data.');
+  }
+};
+
+var onWsConnClose = function(reasonCode, description) {
+  //console.log('Peer disconnected with reason: ' + reasonCode);
+};
+
+var onWsRequest = function(request) {
+  var connection = request.accept('', request.origin);
+
+  //console.log("[2]: onWsRequest");
+  //console.log("[3]: resource: " + request.resource);
+
+  route(request.resource, connection, handlers, clients);
+
+  connection.on('message', onWsConnMessage);
+  connection.on('close', onWsConnClose);
+};
+
+var onWsConnect = function(webSocketConnection) {
+  //console.log("[1]: onWsConnect");
+
+  //webSocketConnection.on('message', onWsConnMessage);
+  //webSocketConnection.on('close', onWsConnClose);
+};
 
 function start(route, handlers) {
   function onRequest(request, response) {
@@ -18,63 +71,42 @@ function start(route, handlers) {
     response.end();
   }
 
-  var server = http.createServer(onRequest).listen(80, function() {
-     console.log("Server has started and is listening on port 80.");
+  if (cluster.isMaster) {
+
+      // Count the machine's CPUs
+      var cpuCount = require('os').cpus().length;
+      var addresses = [];
+      var DEFAULT_PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+
+      console.info('CPUs: ' + cpuCount);
+
+      // Create a worker for each CPU
+      var i;
+      for (i = 0; i < cpuCount ; i++) {
+          var port = DEFAULT_PORT + i;
+          cluster.fork({PORT: port});
+          addresses.push({
+              host: '127.0.0.1',
+              port: port
+          });
+      }
+
+      return true;
+  }
+
+  // arguments to child processes
+  var port = process.env['PORT'];
+
+  var server = http.createServer(onRequest).listen(port, '127.0.0.1', function() {
+      var workerinfo = "";
+      if (cluster.isWorker) workerinfo = ", on worker " + cluster.worker.id;
+      console.info('WoT.City server listening on port ' + port + workerinfo);
   });
 
   wsServer = new WebSocketServer({
     httpServer: server,
     autoAcceptConnections: false
   });
-
-  var push = function(path, data) {
-    var connections = clients[path];
-
-    if (typeof connections === 'undefined')
-        return;
-
-    //console.log('Pushing [' + data + '] to ' + path);
-
-    for (var i = 0; i < connections.length; i++) {
-        connections[i].sendUTF(data);
-    }
-  }
-
-  var onWsConnMessage = function(message) {
-    if (message.type == 'utf8') {
-      //console.log('onWsConnMessage: ' + this.pathname);
-      //console.log('Received: ' + message.utf8Data);
-
-      // Is it a sender ? Yes, then push data to all viewers.
-      if (typeof (this.viewer) !== 'undefined')
-          push(this.viewer, message.utf8Data);
-    } else if (message.type == 'binary') {
-      console.log('Received binary data.');
-    }
-  };
-
-  var onWsConnClose = function(reasonCode, description) {
-    //console.log('Peer disconnected with reason: ' + reasonCode);
-  };
-
-  function onWsRequest(request) {
-    var connection = request.accept('', request.origin);
-
-    //console.log("[2]: onWsRequest");
-    //console.log("[3]: resource: " + request.resource);
-
-    route(request.resource, connection, handlers, clients);
-
-    connection.on('message', onWsConnMessage);
-    connection.on('close', onWsConnClose);
-  }
-
-  function onWsConnect(webSocketConnection) {
-    //console.log("[1]: onWsConnect");
-
-    //webSocketConnection.on('message', onWsConnMessage);
-    //webSocketConnection.on('close', onWsConnClose);
-  }
 
   wsServer.on('request', onWsRequest);
   wsServer.on('connect', onWsConnect);
