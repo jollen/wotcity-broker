275fba4c0b8480f38ad532be25edc13599179b1a
Jollen Chen <jollen@moko365.com>
2015-03-16 19:00:03 +0800

    fixed

--- server.js
+++ server.js
@@ -7,58 +7,6 @@ var WebSocketServer = require('websocket').server;
 // Connected WebSocket clients
 var clients = [];
 
-/**************************************************
- * handlers
- **************************************************/
-var push = function(path, data) {
-  var connections = clients[path];
-
-  if (typeof connections === 'undefined')
-      return;
-
-  //console.log('Pushing [' + data + '] to ' + path);
-
-  for (var i = 0; i < connections.length; i++) {
-      connections[i].sendUTF(data);
-  }
-}
-
-var onWsConnMessage = function(message) {
-  if (message.type == 'utf8') {
-    //console.log('onWsConnMessage: ' + this.pathname);
-    //console.log('Received: ' + message.utf8Data);
-
-    // Is it a sender ? Yes, then push data to all viewers.
-    if (typeof (this.viewer) !== 'undefined')
-        push(this.viewer, message.utf8Data);
-  } else if (message.type == 'binary') {
-    console.log('Received binary data.');
-  }
-};
-
-var onWsConnClose = function(reasonCode, description) {
-  //console.log('Peer disconnected with reason: ' + reasonCode);
-};
-
-var onWsRequest = function(request) {
-  var connection = request.accept('', request.origin);
-
-  //console.log("[2]: onWsRequest");
-  //console.log("[3]: resource: " + request.resource);
-
-  route(request.resource, connection, handlers, clients);
-
-  connection.on('message', onWsConnMessage);
-  connection.on('close', onWsConnClose);
-};
-
-var onWsConnect = function(webSocketConnection) {
-  //console.log("[1]: onWsConnect");
-
-  //webSocketConnection.on('message', onWsConnMessage);
-  //webSocketConnection.on('close', onWsConnClose);
-};
-
 function start(route, handlers) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
@@ -107,6 +55,58 @@ function start(route, handlers) {
     httpServer: server,
     autoAcceptConnections: false
   });
+
+  /**
+   * handlers
+   */
+  var onWsRequest = function(request) {
+    var connection = request.accept('', request.origin);
+
+    //console.log("[2]: onWsRequest");
+    //console.log("[3]: resource: " + request.resource);
+
+    route(request.resource, connection, handlers, clients);
+
+    connection.on('message', onWsConnMessage);
+    connection.on('close', onWsConnClose);
+  };
+
+  var onWsConnect = function(webSocketConnection) {
+    //console.log("[1]: onWsConnect");
+
+    //webSocketConnection.on('message', onWsConnMessage);
+    //webSocketConnection.on('close', onWsConnClose);
+  };
+
+  var push = function(path, data) {
+    var connections = clients[path];
+
+    if (typeof connections === 'undefined')
+        return;
+
+    //console.log('Pushing [' + data + '] to ' + path);
+
+    for (var i = 0; i < connections.length; i++) {
+        connections[i].sendUTF(data);
+    }
+  }
+
+  var onWsConnMessage = function(message) {
+    if (message.type == 'utf8') {
+      //console.log('onWsConnMessage: ' + this.pathname);
+      //console.log('Received: ' + message.utf8Data);
+
+      // Is it a sender ? Yes, then push data to all viewers.
+      if (typeof (this.viewer) !== 'undefined')
+          push(this.viewer, message.utf8Data);
+    } else if (message.type == 'binary') {
+      console.log('Received binary data.');
+    }
+  };
+
+  var onWsConnClose = function(reasonCode, description) {
+    //console.log('Peer disconnected with reason: ' + reasonCode);
+  };
 
   wsServer.on('request', onWsRequest);
   wsServer.on('connect', onWsConnect);
